e800d175104bafa26c4f9ce53d290e2c0f125ad5
harshitsingh2007 <harshit971897@gmail.com>
2025-04-25 22:10:51 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { MainPage } from './MainPage/MainPage';
 import './App.css'
 import React from 'react';
 import '../src/MainPage/Navbar.css'
-import TemplateMain from './Template/TemplateMain'
-import ContactUs from './ContactUs/ContactUs';
 function App() {
   return (
-    // <MainPage/>
-    // <TemplateMain/>
-    <ContactUs/>
+    <MainPage/>
   ); 
 }
 export default App;
